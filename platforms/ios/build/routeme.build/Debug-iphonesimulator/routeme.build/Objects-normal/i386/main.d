f6a89408f8d14cf104a223cf6556d52cd545356e
Ahmed Aly <alyahmed220@gmail.com>
2015-08-26 21:45:46 -0400

    Initial Commit

---
+++ platforms/ios/build/routeme.build/Debug-iphonesimulator/routeme.build/Objects-normal/i386/main.d
@@ -0,0 +1,6 @@
+dependencies: \
+  /Users/ahmedaly/Desktop/Work/route-me/platforms/ios/routeme/main.m \
+  /Users/ahmedaly/Desktop/Work/route-me/platforms/ios/NativeScript/include/NativeScript.h \
+  /Users/ahmedaly/Desktop/Work/route-me/platforms/ios/NativeScript/include/TNSRuntime.h \
+  /Users/ahmedaly/Desktop/Work/route-me/platforms/ios/NativeScript/include/TNSRuntime+Inspector.h \
+  /Users/ahmedaly/Desktop/Work/route-me/platforms/ios/routeme/TNSDebugging.h
